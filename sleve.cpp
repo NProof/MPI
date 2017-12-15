b0e0a345e2cc84563a591e1dec64ea47f26c92ac
NProof <34260623+NProof@users.noreply.github.com>
2017-12-15 13:20:05 +0800

    Update sleve.cpp

--- sleve.cpp
+++ sleve.cpp
@@ -4,15 +4,14 @@
 #include <ctime>
 #include <cstdio>
 
-std::bitset<500000005> marks;
-std::vector<int> result ;
+#define MAXSIZE 500000005
+
+std::bitset< MAXSIZE > marks;
 	
-std::vector<int> findUnderPrimes(int n){
-	result.clear();
+void findUnderPrimes(int n){
 	marks.reset();
 	int k = 2;
 	while( k * k <= n ){
-		result.push_back(k);
 		for(int i =  k * k ; i <= n; i += k){
 			marks.set(i);
 		}
@@ -21,17 +20,11 @@ std::vector<int> findUnderPrimes(int n){
 		}
 		while(marks.test(k));
 	}
-	while(k <= n){
-		if(!marks.test(k))
-			result.push_back(k);
-		k++;
-	}
-	return result;
 }
 
 int main(int argc, char **argv){
 	#ifdef FILEOUT
-		freopen("sleve2.out", "w", stdout);
+		freopen("sleve.out", "w", stdout);
 	#endif
 	clock_t start, end;
 	if(argc > 1){
@@ -39,14 +32,14 @@ int main(int argc, char **argv){
 		for(int i = 1; i < argc; i++){
 			start = clock();
 			sscanf(argv[i], "%d", &x);
-			std::vector<int> underPrimes = findUnderPrimes(x);
+			findUnderPrimes(x);
 //			for (std::vector<int>::iterator it = underPrimes.begin(); it != underPrimes.end(); ++it)
 //				std::cout << ' ' << *it;
 //			std::cout << '\n';
 			end = clock();
 			std::cout << "x : " << x <<
 			", time : " << ( (double) ( end - start ) / CLOCKS_PER_SEC ) << " seconds " <<
-			", size : " << underPrimes.size() << std::endl;
+			", size : " << x - marks.count() - 1 << std::endl;
 		}
 	}
 	return 0;
