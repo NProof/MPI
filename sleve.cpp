c0a87924e9a435845aa9455426d9a1bc34a09d47
NProof <34260623+NProof@users.noreply.github.com>
2017-12-19 15:53:59 +0800

    Update sleve.cpp

--- sleve.cpp
+++ sleve.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
-#include <bitset>
 #include <ctime>
 #include <cstdio>
 
@@ -35,19 +33,15 @@ int main(int argc, char **argv){
 	#ifdef FILEOUT
 		freopen("sleve.out", "w", stdout);
 	#endif
-	clock_t start, end;
+	clock_t start;
 	if(argc > 1){
 		int x ;
 		for(int i = 1; i < argc; i++){
 			start = clock();
 			sscanf(argv[i], "%d", &x);
 			int primes = findUnderPrimes(x);
-//			for (std::vector<int>::iterator it = underPrimes.begin(); it != underPrimes.end(); ++it)
-//				std::cout << ' ' << *it;
-//			std::cout << '\n';
-			end = clock();
 			std::cout << "x : " << x <<
-			", time : " << ( (double) ( end - start ) / CLOCKS_PER_SEC ) << " seconds " <<
+			", time : " << ( (double) ( clock() - start ) / CLOCKS_PER_SEC ) << " seconds " <<
 			", size : " << primes << std::endl;
 		}
 	}
