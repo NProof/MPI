4a1f715244e474e4f807b3abb33fb39721e49e38
NProof <34260623+NProof@users.noreply.github.com>
2017-12-21 18:18:30 +0800

    Update floyd.c

--- floyd.c
+++ floyd.c
@@ -4,10 +4,11 @@
 #include <time.h>
 
 int main(int argc, char **argv){
-	clock_t start;
+	clock_t start = clock();
 	
 //	double  **distances = (double  **) malloc(5 * sizeof(double  *));
 //	for(int i=0; i<5; i++) distances[i] = (unsigned int *) malloc(5 * sizeof(unsigned int));
+
 	double  distances[6][6] = 
 	{{0,2,5,NAN,NAN,NAN},
 	{NAN,0,7,1,NAN,8},
@@ -15,7 +16,25 @@ int main(int argc, char **argv){
 	{NAN,NAN,NAN,0,3,NAN},
 	{NAN,NAN,2,NAN,0,3},
 	{NAN,5,NAN,2,4,0}};
+
+	for(int k=0; k<6; k++) {
+		for(int i=0; i<6; i++) {
+			for(int j=0; j<6; j++) {
+				distances[i][j] = fmin(distances[i][j],distances[i][k]+distances[k][j]);
+			}
+		}
+	}
 	
-	printf("time : %lf seconds\n", (double) ( clock() - start ) / CLOCKS_PER_SEC);
+	for(int i=0; i<6; i++) {
+		for(int j=0; j<6; j++) {
+			if(isnan(distances[i][j]))
+				printf(" NaN");
+			else
+				printf(" %3.lf", distances[i][j]);
+		}
+		putchar('\n');
+	}
+	
+	printf("time : %f seconds\n", (float)( clock() - start ) / CLOCKS_PER_SEC);
 	return 0;
 }
