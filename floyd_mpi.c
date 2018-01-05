f08b2112ebef7985601b8eb08bf5bf614b824255
NProof <34260623+NProof@users.noreply.github.com>
2018-01-05 14:14:43 +0800

    Update floyd_mpi.c

--- floyd_mpi.c
+++ floyd_mpi.c
@@ -21,81 +21,76 @@ int main(int argc, char **argv){
 	// printf("* %d [ %d , %d ]\n", id, id/6, id%6);
 	
 	#define ROWS
-	#define COLS
-	#define COM
+	// #define COLS
+	// #define COM
+	// #define CAN
+	// #define SHO
 	#define PRI
 	
-	
-	for(int k=0; k<6; k++) {
+	for(int t=0; t<1; t++) {
 		#ifdef ROWS
-		if(id%6 == k){
-			for(int j=0; j<6; j++) {
-				if(j != k && j != id/6){
-					MPI_Request request;
-					MPI_Isend (&distances[id/6][id%6], 1, MPI_FLOAT, (id/6)*6+j, 2*k+1, MPI_COMM_WORLD, &request);
-					#ifdef PRI
-					printf("%2d send to %2d when k is %d and *row*\n", id, (id/6)*6+j, k);
-					#endif
-				}
-			}
-		}
-		else{
-			if(id/6 != k){
-				MPI_Request request;
-				MPI_Irecv(&distances[id/6][k], 1, MPI_FLOAT, (id/6)*6+k, 0, MPI_COMM_WORLD, &request);
+		for(int j=0; j<6; j++) {
+			if(j != id/6){
+				MPI_Request requestIsend;
+				MPI_Isend (&distances[id/6][id%6], 1, MPI_FLOAT, (id/6)*6+j, (id/6)*6+j, MPI_COMM_WORLD, &requestIsend);
 				#ifdef PRI
-				printf("%2d re from %2d when k is %d and *row*\n", id, (id/6)*6+k, k);
+				printf("%2d send to %2d when t is %d and *row*\n", id, (id/6)*6+j, t);
+				#endif
+				
+				MPI_Request requestIrecv;
+				MPI_Irecv(&distances[id/6][j], 1, MPI_FLOAT, (id/6)*6+j, (id/6)*6+j, MPI_COMM_WORLD, &requestIrecv);
+				#ifdef PRI
+				printf("%2d re from %2d when t is %d and *row*\n", id, (id/6)*6+j, t);
 				#endif
 			}
 		}
 		#endif
 		// MPI_Barrier(MPI_COMM_WORLD);
 		#ifdef COLS
-		if(id/6 == k){
-			for(int i=0; i<6; i++) {
-				if(i != k && i != id%6){
-					MPI_Request request;
-					MPI_Isend (&distances[id/6][id%6], 1, MPI_FLOAT, i*6+(id%6), 2*k, MPI_COMM_WORLD, &request);
-					#ifdef PRI
-					printf("%2d send to %2d when k is %d and *col*\n", id, i*6+(id%6), k);
-					#endif
-				}
-			}
-		}
-		else{
-			if(id%6 != k){
-				MPI_Request request;
-				MPI_Irecv(&distances[k][id%6], 1, MPI_FLOAT, k*6+(id%6), 0, MPI_COMM_WORLD, &request);
+		for(int i=0; i<6; i++) {
+			if(i != id%6){
+				MPI_Request requestIsend;
+				MPI_Isend (&distances[id/6][id%6], 1, MPI_FLOAT, i*6+(id%6), i*6+(id%6), MPI_COMM_WORLD, &requestIsend);
 				#ifdef PRI
-				printf("%2d re from %2d when k is %d and *col*\n", id, k*6+(id%6), k);
+				printf("%2d send to %2d when t is %d and *col*\n", id, i*6+(id%6), t);
+				#endif
+				
+				MPI_Request requestIrecv;
+				MPI_Irecv(&distances[i][id%6], 1, MPI_FLOAT, i*6+(id%6), i*6+(id%6), MPI_COMM_WORLD, &requestIrecv);
+				#ifdef PRI
+				printf("%2d re from %2d when t is %d and *col*\n", id, i*6+(id%6), t);
 				#endif
 			}
 		}
-		#endif
-		#ifdef COM
-		// MPI_Barrier(MPI_COMM_WORLD);
-		if(!isnan(distances[id/6][k]) && !isnan(distances[k][id%6]))
-			if(isnan(distances[id/6][id%6]) || distances[id/6][id%6] > distances[id/6][k]+distances[k][id%6]){
-				distances[id/6][id%6] = distances[id/6][k]+distances[k][id%6];
-				#ifdef PRI
-				printf("id %d : %f when k %d\n", id, distances[id/6][id%6], k);
-				#endif
-			}
 		#endif
 		MPI_Barrier(MPI_COMM_WORLD);
-		// for(int i=0; i<6; i++) {
-			// for(int j=0; j<6; j++) {
-				// if(isnan(distances[i][j]))
-					// printf(" NaN");
-				// else
-					// printf(" %3.lf", distances[i][j]);
-			// }
-			// putchar('\n');
-		// }
-		
+		#ifdef SHO
+		for(int i=0; i<6; i++) {
+			for(int j=0; j<6; j++) {
+				if(isnan(distances[i][j]))
+					printf(" NaN");
+				else
+					printf(" %3.lf", distances[i][j]);
+			}
+			putchar('\n');
+		}
+		putchar('\n');
+		#endif
+		MPI_Barrier(MPI_COMM_WORLD);
+		#ifdef COM
+		for(int k=0; k<6; k++) {
+			if(!isnan(distances[id/6][k]) && !isnan(distances[k][id%6]))
+				if(isnan(distances[id/6][id%6]) || distances[id/6][id%6] > distances[id/6][k]+distances[k][id%6]){
+					distances[id/6][id%6] = distances[id/6][k]+distances[k][id%6];
+					#ifdef CAN
+					printf("id %d : %f when k %d\n", id, distances[id/6][id%6], k);
+					#endif
+				}
+		}
+		#endif
 	}
 	
-	// printf("distance is %5.0f in %d\n", distances[id/6][id%6], id);
+	printf("distance is %5.0f in %d\n", distances[id/6][id%6], id);
 	
 	MPI_Finalize();
 	return 0;
